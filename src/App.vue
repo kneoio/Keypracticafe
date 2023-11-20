47296481fbdc4f90944fe675cd8129289e6c2552
Aida <justaidajam@gmail.com>
2023-11-20 22:56:54 +0200

    #

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainWorkspace msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainWorkspace from "@/components/MainWorkspace.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainWorkspace
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Keycloak from 'keycloak-js';
 
-createApp(App).mount('#app')
+let keycloak = new Keycloak('/keycloak.json');
+
+keycloak.init({
+    onLoad: 'check-sso',
+    pkceMethod: 'S256'
+}).then((auth) => {
+    if (!auth) {
+        //window.location.reload();
+        console.log("Not Authenticated");
+    } else {
+        console.log("Authenticated");
+    }
+}).catch(() => {
+    console.log("Could not authenticate");
+});
+
+const app = createApp(App)
+app.config.globalProperties.$keycloak = keycloak
+app.mount('#app')
