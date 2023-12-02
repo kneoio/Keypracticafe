5188d823b9dc1630339ff373a38efe7339cda8e3
Aida <justaidajam@gmail.com>
2023-12-02 10:01:47 +0200

    #

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    public: 'www.keypractica.com'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/'
+      : '/'
 })
